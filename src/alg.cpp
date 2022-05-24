10a8ed0cdc772cfc112bf34e684298626629a330
Youth27 <94329773+Youth27@users.noreply.github.com>
2022-05-24 21:46:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,24 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+  BST<std::string> tree;
+  std::ifstream file(filename);
+  std::string simbol = "";
+  if (!file) {
+    throw std::string("Error! ");
+}
+while (!file.eof()) {
+  simbol = "";
+  while (true) {
+    char a = file.get();
+    if ((a >= 65 && a <= 90) || (a >= 97 && a <= 122)) {
+    simbol += tolower(a); 
+    } else {
+      break;
+  }
+}
+tree.add(simbol);
+}
+return tree;
+}
 }
